56869de65e12999ff335ff08fd41bbab6198f306
benzenergy <161916636+benzenergy@users.noreply.github.com>
2026-02-20 13:04:57 +0400

    Update other.java

--- other.java
+++ other.java
@@ -1,9 +1,8 @@
-package com.example.myproject;
-
+package com.benzenergy.benzconfig;
 import java.util.Scanner;
 
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("BenzConfig");
     }
 }
